083f86e8365e83f9edaf086a888abf79b9f04993
powerpress0809 <powerpress0809@naver.com>
2024-02-08 13:59:15 +0900

    dadasdasdaa

--- src/component/member/Frindlist.js
+++ src/component/member/Frindlist.js
@@ -1,16 +1,40 @@
 import MemberHeader from "./MemberHeader";
-import React from "react";
+import React, { useState } from "react";
 import BootPath from "./../../BootPath";
 import { useContext } from "react";
+import axios from "axios";
+import ReactTable from "react-table";
 function Frindlist() {
   const { bootpath } = useContext(BootPath);
+
+  const [data, setData] = useState(null);
+  const getData = async () => {
+    axios
+      .get(bootpath + "/member/friend/list?member_no=2")
+      .then((res) => {
+        setData(res.data);
+        console.log("성공");
+      })
+      .catch((Error) => {
+        console.log(Error);
+      });
+  };
+
   return (
     <>
       <MemberHeader />
       <div className="sub">
         <div className="size">
           <h3 className="sub_title">친구목록</h3>
-          {/*테스트 서버*/}
+          <ReactTable
+            data={data}
+            columns={[
+              {
+                Header: "Name",
+                accessor: "name",
+              },
+            ]}
+          />
         </div>
       </div>
     </>
