ecabcf8e5690dc8fde90f80538dabe8481fcdda0
rlaqudcjs96 <rlaqudcjs96@gmail.com>
2024-02-16 17:46:37 +0900

    참여한 펀딩 진행중/완료 끝

--- src/component/funding/FundingJoinListEnd.js
+++ src/component/funding/FundingJoinListEnd.js
@@ -1,30 +1,14 @@
-import FundingHeader from "./FundingHeader";
 import React, { useState, useEffect, useContext } from "react";
 import axios from "axios";
-import { Link, useNavigate, useSearchParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import bootPath from "../../BootPath";
-import CommonImagePath from "../../commonImagePath";
 import RegistedImagePath from "../../registedImagePath";
-import $ from "jquery";
-function JoinFundingList() {
+
+const FundingJoinListOnGoing = ({ render, setRender }) => {
   const { bootpath } = useContext(bootPath);
-  const { registedImagePath } = useContext(RegistedImagePath);
   const [data, setData] = useState([]);
-  const selectList = [
-    { no: 0, name: "진행중", link: "onGoing" },
-    {
-      no: 1,
-      name: "완료",
-      link: "end",
-    },
-  ];
-  const [selected, setSelected] = useState({
-    no: 1,
-    name: "완료",
-    link: "end",
-  });
-  console.log(selected);
   const member_no = sessionStorage.getItem("no");
+  const { registedImagePath } = useContext(RegistedImagePath);
   const getApi = async () => {
     axios
       .get(`${bootpath}/funding/member/join/end?member_no=${member_no}`)
@@ -35,15 +19,6 @@ function JoinFundingList() {
   useEffect(() => {
     getApi();
   }, []);
-
-  const move = (e) => {
-    setSelected(selectList[e.target.value]);
-    console.log(e.target.value);
-    console.log(selectList[e.target.value]);
-    window.location.href =
-      "/funding/member/join/" + selectList[e.target.value].link;
-  };
-
   return (
     <>
       <div>
@@ -63,10 +38,8 @@ function JoinFundingList() {
                     <img src={registedImagePath + Data.photo} width="100" />
                     <div>펀드명 : {Data.title}</div>
                     <div>내가 낸 금액 : {Data.myPayAmount}</div>
-                    <div>전체 모인 금액: {Data.totalPayAmount}</div>
-                    <div>상태 : {Data.stateMessage}</div>
-                    <div>남은 일수 : {Data.dueDateLeft}</div>
-                    <div>상태 색 : {Data.color}</div>
+                    <div>내가 받은 금액: {Data.settlementAmount}</div>
+                    <div>메세지 : {Data.message}</div>
                   </Link>
 
                   <br />
@@ -82,6 +55,6 @@ function JoinFundingList() {
       </div>
     </>
   );
-}
+};
 
-export default JoinFundingList;
+export default FundingJoinListOnGoing;
