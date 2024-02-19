cc9857ccd9a2bda3fb1633c08a8a9d734460bc2f
jhyoo <jhyoo1224@gmail.com>
2024-02-19 17:31:35 +0900

    성공 실패 투표 기능 구현(테스트는 했는데 깨끗한 데이터로 해볼 필요 있음)

--- src/component/funding/fundingInfoButtonList/MessageNo4.js
+++ src/component/funding/fundingInfoButtonList/MessageNo4.js
@@ -1,12 +1,62 @@
 import BootPath from "./../../../BootPath";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
+import axios from "axios";
+
 const MessageNo4 = ({ obj }) => {
+  console.log(obj);
   const { bootpath } = useContext(BootPath);
+  const [formData, setFormData] = useState({
+    //post 전송할 데이터 필드. giveup 필드값을 바꾸기 위해 펀딩의 no와 현재 로그인한 member_no를 담음
+    fundingNo: obj.fundingNo,
+    memberNo: obj.fundingMemberNo,
+  });
+  const voteSuccess = async () => {
+    try {
+      const response = await axios.post(
+        bootpath + "/fund/voteSuccess",
+        formData
+      );
+
+      console.log("서버 응답:" + response.data.result);
+      console.log(response.data);
+      if (response.data.result === "vote_success") {
+        console.log("성공에 투표 완료" + response.data.giveUp);
+      } else if (response.data.result === "fail") {
+        alert("투표 안됨");
+      } else if (response.data.result === "vote_success_end") {
+        alert("내가 마지막으로 투표해서 정산 상태로 변함");
+      }
+    } catch (error) {
+      console.error("에러 발생:", error);
+    }
+  };
+  const voteFail = async () => {
+    try {
+      const response = await axios.post(bootpath + "/fund/voteFail", formData);
+
+      console.log("서버 응답:" + response.data.result);
+      console.log(response.data);
+      if (response.data.result === "vote_success") {
+        console.log("성공에 투표 완료" + response.data.giveUp);
+      } else if (response.data.result === "fail") {
+        alert("투표 안됨");
+      } else if (response.data.result === "vote_success_end") {
+        alert("내가 마지막으로 투표해서 정산 상태로 변함");
+      }
+    } catch (error) {
+      console.error("에러 발생:", error);
+    }
+  };
   return (
     <>
-      <Link className="btn">성공</Link>
-      <Link className="btn">실패</Link>
+      <Link className="btn" onClick={voteSuccess}>
+        성공
+      </Link>
+      <Link className="btn" onClick={voteFail}>
+        실패
+      </Link>
     </>
   );
 };
