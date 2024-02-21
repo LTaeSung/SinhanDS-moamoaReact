ef725cbc9661c4789ab14046dbda4c854ee47fc2
jhyoo <jhyoo1224@gmail.com>
2024-02-21 16:38:13 +0900

    makefunding에 결제일 선택박스 사라진거 복구

--- src/component/funding/MakeFunding.js
+++ src/component/funding/MakeFunding.js
@@ -22,6 +22,38 @@ function MakeFunding() {
   const [selectedFile, setSelectedFile] = useState(null);
   const [uploadedImageUrl, setUploadedImageUrl] = useState(null);
 
+  const dayOptions = [
+    "---",
+    1,
+    2,
+    3,
+    4,
+    5,
+    6,
+    7,
+    8,
+    9,
+    10,
+    11,
+    12,
+    13,
+    14,
+    15,
+    16,
+    17,
+    18,
+    19,
+    20,
+    21,
+    22,
+    23,
+    24,
+    25,
+    26,
+    27,
+    28,
+  ];
+
   const handleImageClick = () => {
     document.getElementById("file").click();
   };
@@ -80,6 +112,10 @@ function MakeFunding() {
       } else if (param.monthly_payment_amount < 1) {
         alert("매월 결제 금액은 1원 이상이어야 합니다.");
         e.preventDefault();
+      } else if (param.monthly_payment_date === "===") {
+        alert("매월 결제일은 1일에서 28일까지만 선택 가능합니다.");
+        e.preventDefault();
+        return;
       } else if (!("payment_no" in param) || param.payment_no === "") {
         alert("정기 결제 될 카드를 선택해주세요.");
         e.preventDefault();
@@ -139,13 +175,17 @@ function MakeFunding() {
           </div>
           <div className={"paydate"}>
             <p id={"title_tag"}>결제 날짜(매월)</p>
-            <input
-              id={"dateinput"}
-              type="text"
-              name="monthly_payment_date"
-              //value="1"
+            <select
               onChange={handleChange}
-            />
+              //type="text"
+              name="monthly_payment_date"
+            >
+              {dayOptions.map((option, idx) => (
+                <option key={idx} value={idx}>
+                  {option}
+                </option>
+              ))}
+            </select>
             <p id={"title_tag_back"}>일</p>
           </div>
           <AddCardToFund onSelectCard={handleSelectCard} />
