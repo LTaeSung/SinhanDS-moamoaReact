07d7f22690947c9f47971f863a1483d6d2ca5cef
jhyoo <jhyoo1224@gmail.com>
2024-02-21 09:26:43 +0900

    완료된 참여자목록에 정산받은금액 뜸

--- src/component/funding/FundingMember.js
+++ src/component/funding/FundingMember.js
@@ -33,7 +33,12 @@ function FundingMember() {
             <div key={item.no}>
               {item.giveup === true ? null : item.participationdate ===
                 null ? null : (
-                <p>{item.membername}</p>
+                <>
+                  <p>{item.membername}</p>
+                  {item.settlementamount != null ? (
+                    <p>{item.settlementamount}원</p>
+                  ) : null}
+                </>
               )}
             </div>
           ))}
