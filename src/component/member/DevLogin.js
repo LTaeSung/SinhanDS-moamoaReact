fe1b6e30de15886fed863a3169b2e154467432a0
rlaqudcjs96 <rlaqudcjs96@gmail.com>
2024-02-22 17:50:49 +0900

    console.log제거

--- src/component/member/DevLogin.js
+++ src/component/member/DevLogin.js
@@ -24,9 +24,7 @@ const MyLogin = () => {
     getApi();
   };
   const getApi = () => {
-    console.log(param);
     axios.post(bootpath + "/member/devlogin", param).then((res) => {
-      console.log(res);
       if (res.data.result === "success") {
         sessionStorage.setItem("no", res.data.no);
         sessionStorage.setItem("email", res.data.email);
