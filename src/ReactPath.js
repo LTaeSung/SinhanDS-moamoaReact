07a0e1ca83e77eff0e18cb2cccdf000ae1e6f562
rlaqudcjs96 <rlaqudcjs96@gmail.com>
2024-02-27 11:49:12 +0900

    푸쉬하지말자

--- src/ReactPath.js
+++ src/ReactPath.js
@@ -1,4 +1,4 @@
 import { createContext } from "react";
-//const ReactPath = createContext({ reactpath: "http://3.34.2.176" });
-const ReactPath = createContext({ reactpath: "http://localhost:3000" });
+const ReactPath = createContext({ reactpath: "http://3.34.2.176" });
+// const ReactPath = createContext({ reactpath: "http://localhost:3000" });
 export default ReactPath;
