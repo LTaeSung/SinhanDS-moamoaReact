5c3ae57637f411de04a80280059a2b2aa48285f8
jhyoo <jhyoo1224@gmail.com>
2024-02-27 11:16:18 +0900

    admin css 살짝고침

--- src/BootPath.js
+++ src/BootPath.js
@@ -1,4 +1,5 @@
 import { createContext } from "react";
 //const BootPath = createContext({ bootpath: "http://13.125.244.235:8090" });
 const BootPath = createContext({ bootpath: "http://localhost:8090" });
+
 export default BootPath;
