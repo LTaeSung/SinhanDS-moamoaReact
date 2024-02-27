6c3bc5d07eee39c7591007b73f9598426ea02236
rlaqudcjs96 <rlaqudcjs96@gmail.com>
2024-02-27 12:05:59 +0900

    메인비율

--- src/BootPath.js
+++ src/BootPath.js
@@ -1,4 +1,4 @@
 import { createContext } from "react";
-const BootPath = createContext({ bootpath: "http://54.180.82.138:8090" });
-// const BootPath = createContext({ bootpath: "http://localhost:8090" });
+// const BootPath = createContext({ bootpath: "http://54.180.82.138:8090" });
+const BootPath = createContext({ bootpath: "http://localhost:8090" });
 export default BootPath;
